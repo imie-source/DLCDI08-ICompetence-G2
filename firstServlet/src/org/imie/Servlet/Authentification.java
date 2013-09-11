2732fb9136ad29e1c72777c2c6e8fb8750137026
r2imie <imie@imie-VirtualBox.(none)>
2013-09-11 10:01:09 +0200

    Ajout du filtre (désactivé pour continuer le dev sans authentification) Ajout de la requete dynamique pour le UserDTO + modification de toutes les classes pour rendre l'appel de méthode disponible

---
+++ firstServlet/src/org/imie/Servlet/Authentification.java
@@ -0,0 +1,120 @@
+package org.imie.Servlet;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import org.imie.DAO.UserDAO;
+import org.imie.DTO.UserDTO;
+import org.imie.factory.BaseConcreteFactory;
+import org.imie.service.interfaces.IUserService;
+import org.imie.transactionalFramework.TransactionalConnectionException;
+
+@WebServlet(description = "Authentification du user", urlPatterns = { "/Authentification" })
+public class Authentification extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	/**
+	 * @see HttpServlet#HttpServlet()
+	 */
+	public Authentification() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+
+	}
+	
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request,
+			HttpServletResponse response) throws ServletException, IOException {
+		// TODO forwarder sur la page demandée avant authentification
+		// TODO gérer les hash de mots de passe
+		// TODO gérer la déconnexion : quel choix entre la faire ici et avoir
+		// une servlet qui la gère?
+		// récupération et parsing de la requete
+		String path = ((HttpServletRequest) request).getRequestURI();
+		System.out.println("url demandée dans Authentification" +path);
+		String contextPath = request.getContextPath();
+		// la page demandée
+		
+		String resource = request.getRequestURI();
+		System.out.println(resource);
+		
+		Boolean connected = false;
+
+		// récupération de la valeur du parametre de session du loginButton qui
+		// est fixé sur la page du formulaire
+		String formSubmitName = request.getParameter("valider");
+		IUserService userService = BaseConcreteFactory.getInstance()
+				.createUserService(null);
+		
+		
+	
+
+		UserDTO userDTOToIdentify = new UserDTO();
+		UserDTO userDTOToFound = new UserDTO();
+		HttpSession session = request.getSession();
+		String login = "";
+		String password = "";
+		// création d'un DTO auquel on passe le nom
+		if (request.getParameter("login") != null
+				&& request.getParameter("password") != null) {
+			login = (String) request.getParameter("login");
+			password = (String) request.getParameter("password");
+			
+		}
+		userDTOToIdentify.setIdentifiant(login);	
+		System.out.println(userDTOToIdentify.getIdentifiant());
+		try {
+			userDTOToFound =  userService.getUser(userDTOToIdentify);
+
+		} catch (TransactionalConnectionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		if (userDTOToFound != null) {
+			// System.out.println("pwd base " +userDtoFound.getPassword()
+			// +" password session: "+ password);
+			String pwdBase = userDTOToFound.getMotDePasse().replaceAll("\\s", "");
+			System.out.println("bdd :" + pwdBase);
+
+			System.out.println("pwd :" + password);
+			if (pwdBase.equalsIgnoreCase(password.replaceAll("\\s", ""))) {
+				session.setAttribute("connected", 1);
+				session.setAttribute("user", userDTOToFound);
+				
+				
+				System.out.println("Authentification connected");
+				
+			} else {
+				// TODO redirect vers la page de login avec message d'erreur
+				System.out.println("erreur password");
+			}
+
+		} else {
+			session.setAttribute("connected", 0);
+			System.out.println(" dans authentification not connected");
+
+		}
+		
+	}
+	
+}
--- icompetence/src/org/imie/Launcher.java
+++ icompetence/src/org/imie/Launcher.java
@@ -1,6 +1,10 @@
 package org.imie;
 
+import org.imie.DTO.UserDTO;
 import org.imie.IHM.ConsoleIHM;
+import org.imie.factory.BaseConcreteFactory;
+import org.imie.service.interfaces.IUserService;
+import org.imie.transactionalFramework.TransactionalConnectionException;
 
 
 /**
@@ -17,8 +21,37 @@ public class Launcher {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		ConsoleIHM.getInstance().start();
+		//ConsoleIHM.getInstance().start();
+		IUserService userService = BaseConcreteFactory.getInstance()
+				.createUserService(null);
+		
+		
+	
+
+		UserDTO userDTOToIdentify = new UserDTO();
+		UserDTO userDTOToFound = new UserDTO();
+		
+		userDTOToIdentify.setIdentifiant("youmet");	
+		System.out.println(userDTOToIdentify.getIdentifiant());
+		try {
+			userDTOToFound =  userService.getUser(userDTOToIdentify);
+
+		} catch (TransactionalConnectionException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		if (userDTOToFound != null) {
+			// System.out.println("pwd base " +userDtoFound.getPassword()
+			// +" password session: "+ password);
+			String pwdBase = userDTOToFound.getMotDePasse().replaceAll("\\s", "");
+			System.out.println("mot de passe en base :" + pwdBase);
+			
+			
+			
 	}
 
+	
+}
 	
 }
